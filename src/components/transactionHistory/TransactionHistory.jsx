29a29478354c2fe750564aa6649f217fd5410fed
Tatiana <tatka_co@ukr.net>
2023-09-04 03:19:25 +0300

    start

---
+++ src/components/transactionHistory/TransactionHistory.jsx
@@ -0,0 +1,47 @@
+import PropTypes from 'prop-types';
+import css from './TransactionHistory.styled.css';
+
+export default function TransactionHistory({ items }) {
+  return (
+    <table className={css['transaction-history']}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(({ currency, amount, type, id }) => (
+          <TransactionHistoryItem
+            key={id}
+            type={type}
+            amount={amount}
+            currency={currency}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default function TransactionHistoryItem({ currency, amount, type }) {
+  return (
+    <tr>
+      <td>{type}</td>
+      <td>{amount}</td>
+      <td>{currency}</td>
+    </tr>
+  );
+}
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
